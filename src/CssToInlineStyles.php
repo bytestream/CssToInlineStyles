220b73e71e21f55f06f7d4aeaedd1daca81fe248
Kieran Brahney <kieran@supportpal.com>
2021-11-11 21:33:57 +0000

    fix htmlentities conversion

--- src/CssToInlineStyles.php
+++ src/CssToInlineStyles.php
@@ -216,7 +216,7 @@ class CssToInlineStyles
      */
     protected function convertToHtmlEntities($html)
     {
-        return mb_encode_numericentity($html, [0x80, 0xFFFF, 0, 0xFFFF], 'UTF-8');
+        return mb_encode_numericentity($html, [0x80, 0x10FFFF, 0, 0x1FFFFF], 'UTF-8');
     }
 
     /**
